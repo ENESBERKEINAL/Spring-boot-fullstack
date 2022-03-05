a5228062f8ba98e58faf3d4480d6455622a7f5b8
enesberke.inal <enesberke.inal@trendyol.com>
2022-03-05 17:15:09 +0300

    Post Controller-Service Added

--- src/main/java/com/example/questapp/repos/PostRepository.java
+++ src/main/java/com/example/questapp/repos/PostRepository.java
@@ -4,7 +4,10 @@ import com.example.questapp.entities.Like;
 import com.example.questapp.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 
+    List<Post> findByUserId(Long userId);
 }
---
+++ src/main/java/com/example/questapp/services/PostService.java
@@ -0,0 +1,65 @@
+package com.example.questapp.services;
+
+import com.example.questapp.entities.Post;
+import com.example.questapp.entities.User;
+import com.example.questapp.repos.PostRepository;
+import com.example.questapp.requests.PostCreateRequest;
+import com.example.questapp.requests.PostUpdateRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+
+    private PostRepository postRepository;
+    private UserService userService;
+
+    public PostService(PostRepository postRepository,UserService userService) {
+        this.postRepository = postRepository;
+        this.userService = userService;
+    }
+
+
+    public List<Post> getAllPosts(Optional<Long> userId) {
+        if(userId.isPresent()){
+            return postRepository.findByUserId(userId.get());
+        }else{
+            return postRepository.findAll();
+        }
+    }
+
+    public Post getPostById(Long postId) {
+        return postRepository.findById(postId).orElse(null);
+    }
+
+    public Post createOnePost(PostCreateRequest newPostRequest) {
+        User user = userService.getOneUser(newPostRequest.getUserId());
+        if(user == null)
+            return null;
+        Post toSave = new Post();
+        toSave.setId(newPostRequest.getId());
+        toSave.setText(newPostRequest.getText());
+        toSave.setTitle(newPostRequest.getTitle());
+        toSave.setUser(user);
+        return postRepository.save(toSave);
+    }
+
+    public Post updatePostById(Long postId, PostUpdateRequest updatePost) {
+        Optional<Post> post = postRepository.findById(postId);
+        if(post.isPresent()){
+            Post toUpdate = post.get();
+            toUpdate.setText(updatePost.getText());
+            toUpdate.setTitle(updatePost.getTitle());
+            postRepository.save(toUpdate);
+            return toUpdate;
+        }else
+            return null;
+    }
+
+    public void deletePostById(Long postId) {
+        postRepository.deleteById(postId);
+    }
+}
