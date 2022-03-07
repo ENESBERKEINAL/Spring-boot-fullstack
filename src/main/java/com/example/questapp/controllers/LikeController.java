ea22d8f324bdd87e69e4dcfc3aafdc64e59ec372
enesberke.inal <enesberke.inal@trendyol.com>
2022-03-07 19:15:58 +0300

    Like Controller&Service Added

--- src/main/java/com/example/questapp/controllers/LikeController.java
+++ src/main/java/com/example/questapp/controllers/LikeController.java
@@ -1,5 +1,40 @@
 package com.example.questapp.controllers;
 
+import com.example.questapp.entities.Like;
+import com.example.questapp.requests.LikeCreateRequest;
+import com.example.questapp.services.LikeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/likes")
 public class LikeController {
-    //TODO make controller next
+
+    private LikeService likeService;
+
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+    @GetMapping
+    public List<Like> getAllLikesWithParam(@RequestParam Optional<Long> userId, @RequestParam Optional<Long> postId ){
+        return likeService.getAllLikesWithParam(userId,postId);
+    }
+
+    @GetMapping("/{likeId}")
+    public Like getLikeById(@PathVariable Long likeId){
+        return likeService.getLikeById(likeId);
+    }
+
+    @PostMapping
+    public Like createOneLike(@RequestBody LikeCreateRequest likeCreateRequest){
+        return likeService.createOneLike(likeCreateRequest);
+    }
+
+    @DeleteMapping("/{likeId}")
+    public void deleteOneLikeByLikeId(@PathVariable Long likeId){
+        likeService.deleteOneLikeByLikeId(likeId);
+    }
 }
